975b5c85c5b6e1d981c5d2672fc0b198b7cc31ba
Praveen N <praveenn@visualbi.com>
2018-01-26 18:34:16 +0530

    Slider added

--- src/screens/Questions.js
+++ src/screens/Questions.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text,FlatList} from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import * as firebase from 'firebase'
-import {ReactFireMixin} from 'reactfire';
-
-export default class Questions extends React.Component{
+import { ReactFireMixin } from 'reactfire';
+import Carousel from 'react-native-snap-carousel';
+import CarouselStyle from '../styles/Carousel'
+import styles,{sliderWidth, itemWidth} from '../styles/SliderEntry.style';
+export default class Questions extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            questions:[]
+        this.state = {
+            questions: []
         }
     }
     _keyExtractor = (item, index) => index;
@@ -20,24 +22,104 @@ export default class Questions extends React.Component{
             storageBucket: "vbi-da-vinci.appspot.com",
             messagingSenderId: "685626298086"
         };
-        firebase.initializeApp(config);
-        var ref = firebase.database().ref("questions");
-        ref.on("value",(snapshot)=>{
-            this.setState({questions:snapshot.val()})
+        this.firebase = firebase.initializeApp(config);
+        this.ref = firebase.database().ref("questions");
+        this.ref.limitToFirst(10).on("value", (snapshot) => {
+            this.setState({ questions: snapshot.val() })
         });
-      }
-    render(){
+    }
+    componentWillUnmount() {
+        this.ref.off();
+        this.firebase.delete();
+    }
+    _renderItem({ item, index }) {
+        return (
+            <TouchableOpacity
+                activeOpacity={1}
+                style={styles.slideInnerContainer}
+                onPress={() => { alert(`You've clicked`); }}
+            >
+
+                <View style={styles.textContainer}>
+                   
+                    <Text
+                        style={styles.title}
+                        numberOfLines={2}
+                    >
+                        {item.behaiouralTrait.toUpperCase()}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                    <Text
+                        style={styles.subtitle}
+                        numberOfLines={2}
+                    >
+                        {item.evidence}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+    render() {
         console.log(this.props.navigation.state.params);
         console.log(this.state.questions);
-        if(this.state.questions)
-        return (<View>
-            <FlatList
-            data={this.state.questions}
-            keyExtractor={this._keyExtractor}
-            renderItem={({question}) => <Text>{console.log(question)}</Text>}
-            />
-        </View>);
+        if (this.state.questions)
+            return (<View>
+                <Carousel
+                    ref={(c) => { this._carousel = c; }}
+                    data={this.state.questions}
+                    renderItem={this._renderItem}
+                    sliderWidth={sliderWidth}
+                    itemWidth={itemWidth}
+                />
+            </View>);
         else
-        return(null)
+            return (null)
     }
 }
