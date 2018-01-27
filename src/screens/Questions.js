2c28bfe4404e30a4a9e4a4627d732b9045d43bf5
Praveen N <praveenn@visualbi.com>
2018-01-27 16:48:36 +0530

    Firebase config added

--- src/screens/Questions.js
+++ src/screens/Questions.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
-import * as firebase from 'firebase'
-import { ReactFireMixin } from 'reactfire';
 import Carousel from 'react-native-snap-carousel';
-import CarouselStyle from '../styles/Carousel'
 import styles,{sliderWidth, itemWidth} from '../styles/SliderEntry.style';
+import firebaseDb from '../services/firebase'
+
 export default class Questions extends React.Component {
     constructor(props) {
         super(props);
@@ -12,26 +11,17 @@ export default class Questions extends React.Component {
             questions: []
         }
     }
-    _keyExtractor = (item, index) => index;
+
     componentWillMount() {
-        var config = {
-            apiKey: "AIzaSyClIA9deJhn3rLS3_TgQ1STWy5XZldPs7s",
-            authDomain: "vbi-da-vinci.firebaseapp.com",
-            databaseURL: "https://vbi-da-vinci.firebaseio.com",
-            projectId: "vbi-da-vinci",
-            storageBucket: "vbi-da-vinci.appspot.com",
-            messagingSenderId: "685626298086"
-        };
-        this.firebase = firebase.initializeApp(config);
-        this.ref = firebase.database().ref("questions");
+        this.ref = firebaseDb.ref("questions");
         this.ref.limitToFirst(10).on("value", (snapshot) => {
             this.setState({ questions: snapshot.val() })
         });
     }
     componentWillUnmount() {
         this.ref.off();
-        this.firebase.delete();
     }
+
     _renderItem({ item, index }) {
         return (
             <TouchableOpacity
